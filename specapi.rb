81dbc93233a0be3fa21026dc2565d3581014e36e
Ian Warwick <ian.warwick@just-eat.com>
2014-06-13 10:57:19 +0100

    upped version

--- specapi.rb
+++ specapi.rb
@@ -4,7 +4,7 @@ class Specapi < Formula
   url 'https://github.com/robotoworks/specapi/releases/download/v0.1.1/specapi.tar.gz'
   homepage 'https://github.com/robotoworks/specapi'
   sha1 'f259ce7a80b7f9640155c5657e039593719c706b'
-  version '0.1.1'
+  version '0.1.2'
 
   def install
     prefix.install Dir['*']
