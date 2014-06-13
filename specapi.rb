c18346afc60748d5321e6044a62158fcc9cfc705
Ian Warwick <ian.warwick@just-eat.com>
2014-06-13 15:03:45 +0100

    updated binary

--- specapi.rb
+++ specapi.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Specapi < Formula
   url 'https://github.com/robotoworks/specapi/releases/download/v0.1.2/specapi.tar.gz'
   homepage 'https://github.com/robotoworks/specapi'
-  sha1 '06ad816176bded4b89c7bd9e739f29e5ccc6930c'
+  sha1 '51eca581867f1d898465c45609aeb45eea56044f'
   version '0.1.2'
 
   def install
